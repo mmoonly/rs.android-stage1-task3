74546263dc3f4bae7cd1acc946d0a2231f0bec47
mmoonly <61161823+mmoonly@users.noreply.github.com>
2020-05-05 10:52:20 +0300

    Task 1 complete

--- src/main/kotlin/subtask1/PolynomialConverter.kt
+++ src/main/kotlin/subtask1/PolynomialConverter.kt
@@ -1,9 +1,28 @@
 package subtask1
 
+import java.lang.StringBuilder
+
 class PolynomialConverter {
 
-    // TODO: Complete the following function
-    fun convertToStringFrom(numbers: Array<Int>): String? {
-        throw NotImplementedError("Not implemented")
+    fun converфtToStringFrom(numbers: Array<Int>): String? {
+        if(numbers.isNullOrEmpty()) return null
+        val result = StringBuilder()
+        for(i in numbers.indices){
+            val n = numbers[i]
+            if(n != 0) result.apply {
+                if(n < 0){
+                    append(" - ")
+                } else if(i != 0) append(" + ")
+                if(Math.abs(n) != 1)
+                    append(Math.abs(n))
+                if(i != numbers.indices.last)
+                    append("x")
+                if(i < numbers.size - 2){
+                    append("^")
+                    append(numbers.size - 1 - i)
+                }
+            }
+        }
+        return result.toString()
     }
 }

a1cdfa9cd8ba09770fb49882f1ce81219622dd9a
mmoonly <61161823+mmoonly@users.noreply.github.com>
2020-05-05 11:36:27 +0300

    Update FullBinaryTrees.kt

--- src/main/kotlin/subtask6/FullBinaryTrees.kt
+++ src/main/kotlin/subtask6/FullBinaryTrees.kt
@@ -1,9 +1,77 @@
 package subtask6
 
+import java.util.*
+import javax.swing.tree.TreeNode
+import kotlin.collections.ArrayList
+
 class FullBinaryTrees {
 
-    // TODO: Complete the following function
+    inner class Node{
+        var depth = 0
+        var left: Node? = null
+        var right: Node? = null
+    }
+
+    private fun getTreeAsList(n: Node):ArrayList<Int?>{
+        val result = ArrayList<Int?>()
+        var root = n
+        result.add(0)
+        val maxd = n.depth
+        val children = ArrayList<Node?>()
+        children.add(n.left)
+        children.add(n.right)
+        var currentLevel = 0
+        while(children.isNotEmpty()){
+            val newChildren = ArrayList<Node?>()
+            for(c in children){
+                if(c == null){
+                    if(currentLevel != maxd) result.add(null)
+                } else{
+                    result.add(0)
+                    newChildren.add(c.left)
+                    newChildren.add(c.right)
+                }
+            }
+            children.clear()
+            children.addAll(newChildren)
+            newChildren.clear()
+            currentLevel++
+        }
+        return result
+    }
+
+    fun generateTrees(n: Int): ArrayList<Node>{
+        if(n % 2 == 0) return ArrayList()
+        if(n == 1) return arrayListOf(Node())
+        val result = ArrayList<Node>()
+        for(i in 1..(n-1) step 2){
+            val left = generateTrees(i)
+            val right = generateTrees(n - 1 - i)
+            for(ln in left){
+                for(rn in right){
+                    val root = Node()
+                    root.left = ln
+                    root.right = rn
+                    root.depth = maxOf(ln.depth, rn.depth) + 1
+                    result.add(root)
+                }
+            }
+        }
+        return result
+    }
+
     fun stringForNodeCount(count: Int): String {
-        throw NotImplementedError("Not implemented")
+        val r = generateTrees(count)
+        
+        val result = ArrayList<ArrayList<Int?>>()
+        for(n in r){
+            val l = getTreeAsList(n)
+            while (l[l.size - 1] == null){
+                l.removeAt(l.size - 1)
+            }
+       
+            result.add(l)
+        }
+        return result.toString()
     }
 }
